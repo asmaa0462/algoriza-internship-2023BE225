1dece4030ebbfc49fde66e0a626c4da01db63ca7
asmaa0462 <asmaatarek06@gmail.com>
2023-12-07 03:35:57 +0200

    kye

---
+++ DomainLayer/Entity/Enum/DiscountState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Entity.Enum
+{
+    public enum DiscountState
+    {
+        Active = 0,
+        Deactive = 1,
+    }
+}
---
+++ DomainLayer/Entity/Specialization.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer.Entity
+{
+    public class Specialization
+    {
+        public int SpecializationId { get; set; }
+        public string SpecializationName { get; set; }
+    }
+}
